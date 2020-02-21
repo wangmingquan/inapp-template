79478a80f54aa99485350b6ec3ffe11fe3c7883c
王明全 <wangmingquan@sensorsdata.cn>
2020-02-21 16:07:42 +0800

    所有控件必须有 width

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -2,6 +2,8 @@
   <div class="container">
     <a href="tel:110">110</a>
     <button @click="call110">110</button>
+
+    <div class="text-box">这里是一段乱七八糟的文字，没有设置line-height，各位瞅瞅</div>
     <div class="page">
       <div v-for="item of data" :key="item.id" class="phone">
         <InApp :data="util.data2dom(item)" />
@@ -34,6 +36,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.text-box{
+  padding: 10px;
+  width: 100px;
+  border: #ccc 1px solid;
+}
 .page{
   display:flex;
   padding: 10px 20px;
